065f09816d843c5e8b70ade6b4d00cae280780bb
Christian Llontop <chrisllontop@icloud.com>
2022-06-06 15:55:39 -0500

    feat: replace marked with markdoc

--- src/engines/markdown.ts
+++ src/engines/markdown.ts
@@ -1,5 +1,5 @@
 import { BaseEngine } from "../baseEngine";
-import { marked } from 'marked';
+import Markdoc from '@markdoc/markdoc';
 
 export class Markdown extends BaseEngine implements EngineInterface {
 
@@ -9,28 +9,17 @@ export class Markdown extends BaseEngine implements EngineInterface {
         this.names = ["markdown"];
 
         if(opts) {
-            this.opts = opts;   
-        } else {
-            this.opts = {
-                pedantic: false,
-                gfm: true,
-                breaks: false,
-                sanitize: false,
-                smartLists: true,
-                smartypants: false,
-                xhtml: false
-              };
+            this.opts = opts;
         }
 
-        this.engine = marked;
-
-        this.engine.setOptions(this.opts);
+        this.engine = Markdoc;
 
         this.setExtensions(["md", "markdown"]);
     }
 
     async render(source:string, data?:object): Promise<string> {
-
-        return this.engine.parse(source);
+        const ast = this.engine.parse(source)
+        const content = this.engine.transform(ast, this.opts);
+        return this.engine.renderers.html(content);
     }
-} 
+}
