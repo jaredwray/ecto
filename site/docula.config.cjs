59155eb0c5ad520300ce20257d350f6a37843000
Jared Wray <me@jaredwray.com>
2024-03-07 12:04:59 -0800

    adding in website support with docula

--- site/docula.config.cjs
+++ site/docula.config.cjs
@@ -1,6 +1,6 @@
 const path = require('node:path');
 const process = require('node:process');
-const fsExtra = require('fs-extra');
+const fs = require('node:fs');
 
 module.exports.options = {
 	githubPath: 'jaredwray/ecto',
@@ -12,8 +12,8 @@ module.exports.options = {
 module.exports.onPrepare = async config => {
 	const readmePath = path.join(process.cwd(), './README.md');
 	const readmeSitePath = path.join(config.sitePath, 'README.md');
-	const readme = await fsExtra.readFile(readmePath, 'utf8');
+	const readme = await fs.promises.readFile(readmePath, 'utf8');
 	const updatedReadme = readme.replace('![Ecto](ecto_logo.png "Ecto")\n\n', '');
 	console.log('writing updated readme to', readmeSitePath);
-	await fsExtra.writeFile(readmeSitePath, updatedReadme);
+	await fs.promises.writeFile(readmeSitePath, updatedReadme);
 };
