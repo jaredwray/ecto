93b0ba93565a8fd75336beebbdfba083f676a8e3
Jared Wray <me@jaredwray.com>
2025-06-14 09:44:14 -0700

    Update ecto.test.ts

--- test/ecto.test.ts
+++ test/ecto.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment */
 import fs from 'node:fs';
 import {expect, it} from 'vitest';
 import {Ecto} from '../src/ecto.js';
