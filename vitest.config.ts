b126d3021bd474fc9e364c6a909ac8eff006c0e9
Jared Wray <me@jaredwray.com>
2025-08-19 08:47:50 -0700

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
 	test: {
 		coverage: {
-			reporter: ['lcov'],
+			reporter: ['lcov', 'text'],
 			exclude: [
 				'bin/**',
 				'**/engine-interface**',
